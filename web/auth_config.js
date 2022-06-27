8dd2e6a3ca44be1a617ef8d3e31a551d51373194
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-06-27 09:28:19 +0300

    fix: auth0 config

--- web/auth_config.js
+++ web/auth_config.js
@@ -1,11 +1,11 @@
-// const config = {
-//   domain: 'dev-zzcvxgj4.us.auth0.com',
-//   clientId: 'SDYhLKkZszAyUGCCket4mSBBxWSVdtQM',
-// }; // main config
-
 const config = {
   domain: 'dev-zzcvxgj4.us.auth0.com',
-  clientId: 'URtDxkdUeXd5Mf2jn1BmKUVagtt2my8f',
-}; // auth0 config for dev needs
+  clientId: 'SDYhLKkZszAyUGCCket4mSBBxWSVdtQM',
+}; // main config
+
+// const config = {
+//   domain: 'dev-zzcvxgj4.us.auth0.com',
+//   clientId: 'URtDxkdUeXd5Mf2jn1BmKUVagtt2my8f',
+// }; // auth0 config for dev needs
 
 export default config;
