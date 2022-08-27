b4434a3b3933fedc23ec9d323a27064b8b13667b
Джихад Хадж Хусейн <skiptomylou999@gmail.com>
2022-08-27 04:23:57 +0300

    fix: add typings

--- web/src/store.ts
+++ web/src/store.ts
@@ -4,11 +4,23 @@ type User = {
   email: string;
 };
 
+interface IGithubUser {
+  aud: string;
+  exp: number;
+  iat: number;
+  iss: string;
+  name: string;
+  nickname: string;
+  picture: string;
+  sub: string;
+  updated_at: string;
+}
+
 export const isAuthenticated = writable(false);
 export const isGithubAuth = writable(false);
 export const isTokenRequested = writable(false);
 export const isChecked = writable(false);
 export const lastRequestedTime = writable(null);
 export const user = writable<User | undefined>();
-export const githubUser = writable({});
+export const githubUser = writable<IGithubUser | {}>({});
 export const popupOpen = writable(false);
